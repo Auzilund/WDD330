e974775cb79e4d14d365203ac1d4341c501b3c79
Auzilund <lundberg572@gmail.com>
2021-10-14 17:46:33 -0600

    Adding week 5 to main index

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const links = [{label: "Week01",url: "week1/week01.html"},{label: "Week02",url: "week2/week02.html"},{label: "Week03",url: "week3/week03.html"},{label: "Week04",url: "week4/week04.html"}];
+const links = [{label: "Week01",url: "week1/week01.html"},{label: "Week02",url: "week2/week02.html"},{label: "Week03",url: "week3/week03.html"},{label: "Week04",url: "week4/week04.html"},{label: "Week05",url: "week5/week05.html"}];
 let list = document.getElementById("tableOfContents");
 
 links.forEach((item)=>{
