481b97caf76deff81dfe0099137e628058e2dcba
Auzilund <lundberg572@gmail.com>
2021-10-29 20:07:09 -0600

    Adding week 7

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const links = [{label: "Week01",url: "week1/week01.html"},{label: "Week02",url: "week2/week02.html"},{label: "Week03",url: "week3/week03.html"},{label: "Week04",url: "week4/week04.html"},{label: "Week05",url: "week5/week05.html"},{label: "Week06",url: "week6/week06.html"}];
+const links = [{label: "Week01",url: "week1/week01.html"},{label: "Week02",url: "week2/week02.html"},{label: "Week03",url: "week3/week03.html"},{label: "Week04",url: "week4/week04.html"},{label: "Week05",url: "week5/week05.html"},{label: "Week06",url: "week6/week06.html"},{label: "Week07",url: "week7/week07.html"}];
 let list = document.getElementById("tableOfContents");
 
 links.forEach((item)=>{
